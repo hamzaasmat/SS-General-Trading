5ef90d10dcb31358c47951cee9f3b62c71475157
Nabia Saroosh <Nabia Saroosh@NABIA>
2017-08-20 22:02:09 +0500

    2nd commit

--- SS General Trading/Models/SSGeneralTrading.Designer.cs
+++ SS General Trading/Models/SSGeneralTrading.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\nabia saroosh\documents\visual studio 2015\Projects\SS General Trading\SS General Trading\Models\SSGeneralTrading.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Nabia Saroosh\Source\Repos\SS-General-Trading2\SS General Trading\Models\SSGeneralTrading.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
