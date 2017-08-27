dd579890be6576b9c03338e41464f743c970ab4d
hamza <hamza@MIRZA_ADIL>
2017-08-27 15:27:02 +0500

    admin complete

---
+++ SS General Trading/Content/admin-js.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $(".edit").html("<span class='glyphicon glyphicon-pencil'></span>").tooltip({ title: "Edit" });
+    $(".detail").html("<span class='glyphicon glyphicon-file'></span>").tooltip({ title: "Details" });
+    $(".delete").html("<span class='glyphicon glyphicon-trash'></span>").tooltip({ title: "Delete" });
+})
