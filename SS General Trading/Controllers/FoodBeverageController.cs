c623deb906c37f964fb3503017ec8678f73d9aca
Nabia Saroosh <Nabia Saroosh@NABIA>
2017-08-27 20:08:03 +0500

    Push Commit1

---
+++ SS General Trading/Controllers/FoodBeverageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SS_General_Trading.Controllers
+{
+    public class FoodBeverageController : Controller
+    {
+        // GET: FoodBeverage
+        public ActionResult FoodBeverage()
+        {
+            return View();
+        }
+    }
+}
